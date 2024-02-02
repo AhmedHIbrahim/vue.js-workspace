e39b370bea4d4bc0da1f01ff54d1509f3d07ef7b
Ahmed <ahmedhibrahim01@gmail.com>
2024-02-02 22:21:22 +0300

    changed demo-app from options to composition api

---
+++ _20--compition-api-demo/README.md
@@ -0,0 +1,1 @@
+![Screen Shot 2024-02-02 at 22 18 10](https://github.com/AhmedHIbrahim/vue-workspace/assets/48028666/fb07a798-6dd0-4bd3-860e-c33158d9f200)
---
+++ _20--compition-api-demo/src/dummy-data.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    id: 'Omar',
+    fullName: 'Omar Yildiz',
+    projects: [
+      { id: 'omar_p1', title: 'Record the Vue course' },
+      { id: 'omar_p2', title: 'Create more programming courses' },
+      { id: 'omar_p3', title: 'Keep content up-to-date' }
+    ]
+  },
+  {
+    id: 'Lorap',
+    fullName: 'Lorap Mirens',
+    projects: [
+      { id: 'lorap_p1', title: 'Create JS courses' },
+      { id: 'lorap_p2', title: 'Dive into Machine Learning topics' },
+      { id: 'lorap_p3', title: 'Other things' }
+    ]
+  },
+  {
+    id: 'Juon',
+    fullName: 'Juon Jiees',
+    projects: [
+      { id: 'juon_p1', title: 'Create agenda for next months' },
+      { id: 'juon_p2', title: 'Explore new topics' }
+    ]
+  },
+  {
+    id: 'Micler',
+    fullName: 'Micler Milhael',
+    projects: []
+  }
+];
